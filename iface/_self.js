56403e34d47eac2dcabe519ef887eaa48ac272c9
Keidrych Anton-Oates <keidrych@sotekton.com>
2019-03-04 20:08:07 +1000

    refactor: condense _selfHelp for core

--- iface/_self.js
+++ iface/_self.js
@@ -1,6 +1,7 @@
 const R = require('rambdax')
 const Skematic = require('skematic')
 const latestSemVer = require('latest-semver')
+const compareVersions = require('semver-compare')
 
 // NOTE: https://github.com/mekanika/skematic for detailed information about how to use
 
@@ -9,11 +10,41 @@ const _selfVersions = {}
 /* PlopInjection:_self */
 _selfVersions['0.1.0'] = {}
 
-module.exports = version => {
-	const _selfLatest = R.defaultTo(latestSemVer(R.keys(_selfVersions)), version)
-	return {
-		version: _selfLatest,
-		schema: {
+function versionList({version = null, direction} = {version: null}) {
+	const trans = R.path('transformations', direction)
+	const index = R.findIndex(transform => {
+		return compareVersions(transform.version, version) <= 0
+	}, trans)
+	return index >= 0 ? trans.slice(index) : trans
+}
+
+function highestCompatible({direction, version}) {
+	const sortedBridges = R.keys(direction.bridgeMapping).sort(compareVersions)
+	const bridgeVersion = (version
+		? R.find(bridge => compareVersions(bridge, version) <= 0, sortedBridges)
+		: sortedBridges.slice(-1))[0]
+	return direction.bridgeMapping[bridgeVersion]
+}
+
+async function result({version, data, direction}) {
+	const mapper = createMapper()
+	const compatibleVersion = highestCompatible({direction, version})
+	R.map(
+		versionMap => versionMap.mapping(mapper),
+		versionList({version: compatibleVersion, direction})
+	)
+	return mapper.executeAsync(data)
+}
+
+module.exports = {
+	_selfHelp: {versionList, highestCompatible, result},
+	schema: ({version}) => {
+		const _selfLatest = R.defaultTo(
+			latestSemVer(R.keys(_selfVersions)),
+			version
+		)
+		return {
+			version: _selfLatest,
 			format: data => Skematic.format(_selfVersions[_selfLatest], data),
 			validate: data => Skematic.validate(_selfVersions[_selfLatest], data)
 		}
